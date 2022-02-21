183b814d38d48c33d112deff4fbce66f0134fc6d
panchanidivu <71247810+panchanidivu@users.noreply.github.com>
2022-02-21 18:26:35 +0530

    update

--- book/src/main/java/com/ebook/book/response/CustomResponseEntity.java
+++ book/src/main/java/com/ebook/book/response/CustomResponseEntity.java
@@ -1,5 +1,7 @@
 package com.ebook.book.response;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,4 +20,9 @@ public class CustomResponseEntity{
 	private String message;
 	
 	private Object data;
+
+	public CustomResponseEntity status(HttpStatus status2) {
+		this.status = status2.value();
+		return this;
+    }
 }
