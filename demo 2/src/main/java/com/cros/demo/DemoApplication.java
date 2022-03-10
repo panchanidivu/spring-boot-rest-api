d8477b0543a2a9d7405900f6c801c26967f67282
panchanidivu <71247810+panchanidivu@users.noreply.github.com>
2022-03-10 10:00:30 +0530

    Update DemoApplication.java

--- demo 2/src/main/java/com/cros/demo/DemoApplication.java
+++ demo 2/src/main/java/com/cros/demo/DemoApplication.java
@@ -1,9 +1,9 @@
 package com.cros.demo;
-
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.function.client.WebClient;
-
+@EnableCaching
 @SpringBootApplication
 public class DemoApplication {
 
